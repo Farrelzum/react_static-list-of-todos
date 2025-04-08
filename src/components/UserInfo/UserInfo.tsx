61b7ac76a75bec81c5e2ebeae3492263608b0412
Grzegorz <grze.pra@gmail.com>
2025-04-08 13:50:58 +0200

    Solution

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,2 +1,17 @@
-// Add the required types and props
-export const UserInfo = () => <>UserInfo markup</>;
+import { Todo } from '../../types/Todo';
+
+interface TodoProps {
+  todo: Todo | null;
+}
+
+export const UserInfo = ({ todo }: TodoProps) => {
+  if (!todo || !todo.user) {
+    return null;
+  }
+
+  return (
+    <a className="UserInfo" href={`mailto:${todo.user.email}`}>
+      {todo.user.name}
+    </a>
+  );
+};
